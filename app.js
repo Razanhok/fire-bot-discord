f2a340d63879d4dd7c941e5bb2cbf0641769138f
Damian Blume <damian.blume@gmail.com>
2017-04-22 15:15:17 +0200

    Changed token and IDs to be obtained by env variable

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 const komada = require("komada");
 
-const mode = process.env.mode;
-let token;
-let clientID;
-
-if (mode === "normal") {
-  token = "MjkzNDU0OTU2MTc2ODY3MzI4.C7G0vA.EAo1wCaxVP-lA_Xgue_apsNS52c";
-  clientID = "293454956176867328";
-} else if (mode === "development") {
-  token = "MjkzODIzMDQ4MzMzNjU2MDc1.C7MLjA.bEE9FxaX32utSG8A7tvqV3N5c4A";
-  clientID = "293823048333656075";
-} else {
-  throw new Error("Invalid environment");
-}
-
-console.log(`Mode : ${mode}`);
-console.log(`Token : ${token}`);
-console.log(`clientID : ${clientID}`);
+// Fire Bot token : "MjkzNDU0OTU2MTc2ODY3MzI4.C7G0vA.EAo1wCaxVP-lA_Xgue_apsNS52c" ID : "293454956176867328"
+// Fire Bot Dev token : "MjkzNDU0OTU2MTc2ODY3MzI4.C7G0vA.EAo1wCaxVP-lA_Xgue_apsNS52c" ID : "293823048333656075"
 
 komada.start({
-  botToken: token,
-  ownerID: "179916759187456010",
-  clientID,
+  botToken: process.env.BOT_TOKEN,
+  ownerID: process.env.OWNER_ID,
+  clientID: process.env.CLIENT_ID,
   prefix: "!",
   clientOptions: {
     fetchAllMembers: true,
