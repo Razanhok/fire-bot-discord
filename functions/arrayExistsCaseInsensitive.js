3ef8da2d68e34a2c2dbecb7bcaa70977d64391a4
Damian Blume <damian.blume@gmail.com>
2017-04-22 15:19:24 +0200

    Remove unused functions

--- functions/arrayExistsCaseInsensitive.js
+++
@@ -1,7 +0,0 @@
-module.exports = (array, string) => {
-  const hasRole = array.some((role) => {
-    if (role.name.toLowerCase() === string.toLowerCase()) return true;
-    return false;
-  });
-  return hasRole;
-};
--- functions/collectionFindCaseInsensitive.js
+++
@@ -1,8 +0,0 @@
-module.exports = (collection, string) => {
-  return collection.find((item) => {
-    if (item.name.toLowerCase() === string.toLowerCase()) {
-      return true;
-    }
-    return false;
-  });
-};
