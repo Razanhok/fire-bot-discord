91196fdad6405bd0763e07a3fa8d5863e4920682
Damian Blume <damian.blume@gmail.com>
2017-05-03 23:35:58 +0200

    Removed logging and comments

--- functions/roleChangerFirebot.js
+++ functions/roleChangerFirebot.js
@@ -5,24 +5,18 @@ const isit = require("isit");
 /* eslint no-param-reassign: "off" */
 
 module.exports = (client, msg, operation, role, roles, possibleRoles) => {
-  // collection.find but case insens
   function collectionFindNoCase(rls, string) {
     return rls.find((item) => {
-      console.log(`In find role in collection no case : item.name.toLowerCase() = ${item.name.toLowerCase()} / string.toLowerCase() = ${string.toLowerCase()}`);
-      console.log(`Value returned to collection.find : ${item.name.toLowerCase() === string.toLowerCase()}`);
       return item.name.toLowerCase() === string.toLowerCase();
     });
   }
 
-// check if user has a role
   function hasRole(member, rl) {
     return member.roles.some((item) => {
       return item.id === rl.id;
     });
   }
-    // end of role list config marker / start of common part between role.js and hero.js ----------------------------
 
-  // make roles be an array with all the roles the user wants
   if (isit("non-empty array", roles)) {
     roles.unshift(role);
   } else {
@@ -31,18 +25,12 @@ module.exports = (client, msg, operation, role, roles, possibleRoles) => {
 
   if (roles.length > 2) msg.channel.startTyping();
 
-  // replace roles with an array of object with info about the requested role : name, discordRole, if it was fixed using levenshtein, if the user already has the role
   roles.forEach((element, index) => {
-    client.funcs.log(`Argument number ${index} : ${roles[index]}`, "debug");
-    // check if the role is in config
     if (!ci(possibleRoles).includes(element)) {
-      client.funcs.log("Raw argument doesn't match a role", "debug");
       if (isit("empty", element)) {
         roles[index] = { invalid: true };
-      } else if (!possibleRoles.some((possRole) => { // checks if the user made a small spelling mistake, and if he did put the corresponding role in the array normally but mention it was "fixed"
-        client.funcs.log(`Levenshtein : ${possRole.toLowerCase()} <=> ${element.toLowerCase()} = ${levenshtein.get(possRole.toLowerCase(), element.toLowerCase())} Limit : ${possRole.length / 2.5}`, "debug");
+      } else if (!possibleRoles.some((possRole) => {
         if (levenshtein.get(possRole.toLowerCase(), element.toLowerCase()) < (possRole.length / 2.5)) {
-          client.funcs.log(`Levenshtein fix found : ${possRole.toLowerCase()} <=> ${element.toLowerCase()} = ${levenshtein.get(possRole.toLowerCase(), element.toLowerCase())}`, "debug");
           const foundRole = collectionFindNoCase(msg.guild.roles, possRole);
           if (!isit("non-empty object", foundRole)) {
             msg.channel.stopTyping(true);
@@ -56,29 +44,23 @@ module.exports = (client, msg, operation, role, roles, possibleRoles) => {
           return true;
         }
         return false;
-      })) { // this is if we couldn't find a matching role
-        client.funcs.log("Role invalid and no fix found", "debug");
-        roles[index] = { name: element }; // roles[index] = { name: roles[index] };
+      })) {
+        roles[index] = { name: element };
       }
-    } else { // this is if the user input matched a config role
-      client.funcs.log("Role matched a role in config", "debug");
+    } else {
       const foundRole = collectionFindNoCase(msg.guild.roles, element);
       if (!isit("non-empty object", foundRole)) {
         msg.channel.stopTyping(true);
         throw new Error("role in config doesn't exist in this server");
       }
-      client.funcs.log("Continued after find role", "debug");
-      client.funcs.log(`Found role : ${foundRole}`, "debug");
       roles[index] = { name: element, role: foundRole, fixed: false, hasAlready: hasRole(msg.member, foundRole) };
       if (!roles[index].role) {
         msg.channel.stopTyping(true);
         throw new Error(`Error : role '${element.name}' from config doesn't exist in this guild. Contact an admin please.`);
       }
     }
-    client.funcs.log("------------------------------------------------------", "debug");
   });
 
-  // removes duplicates and puts them in the duplicates array
   const duplicates = [];
   makeUnique(roles, (a, b) => {
     if (!a.role || !b.role) return false;
@@ -89,33 +71,24 @@ module.exports = (client, msg, operation, role, roles, possibleRoles) => {
     return false;
   });
 
-  client.funcs.log("========================================================", "debug");
   const rolesToAdd = [];
   const rolesToRemove = [];
   const unknownArgs = [];
   const rolesUnchanged = [];
 
-  // gives roles and stuff based on the roles array
   roles.forEach((element) => {
     if (!element.invalid) {
-      client.funcs.log(`Role : ${element.name}`, "debug");
       if (isit("non-empty", element.role)) {
         if (operation === "add") {
-          client.funcs.log("We have an existing role the user wants to add", "debug");
           if (isit("falsey", element.hasAlready)) {
-            client.funcs.log("User doesn't have the role, adding role to add list", "debug");
             rolesToAdd.push(element.role);
           } else {
-            client.funcs.log("User already had the role, nothing being done", "debug");
             rolesUnchanged.push(element.role.name);
           }
         } else if (operation === "remove") {
-          client.funcs.log("We have an existing role the user wants to remove", "debug");
           if (isit("truthy", element.hasAlready)) {
-            client.funcs.log("User has the role, adding role to remove list", "debug");
             rolesToRemove.push(element.role);
           } else {
-            client.funcs.log("User didn't have the role, nothing being done", "debug");
             rolesUnchanged.push(element.role.name);
           }
         } else {
@@ -123,25 +96,17 @@ module.exports = (client, msg, operation, role, roles, possibleRoles) => {
           throw new Error("The operation (add|remove) didn't match these 2 option...");
         }
       } else {
-        client.funcs.log("This role doesn't exist, nothing being done", "debug");
         unknownArgs.push(element.name);
       }
-      client.funcs.log("------------------------------------------------------", "debug");
     }
   });
 
   const rolesChanged = [];
 
-  client.funcs.log(`Roles to add ${rolesToAdd}`, "debug");
-  client.funcs.log(`Roles to remove ${rolesToRemove}`, "debug");
 
   function userReply() {
-    client.funcs.log(`Roles changed : ${rolesChanged}`, "debug");
-    client.funcs.log(`Unknown args : ${unknownArgs}`, "debug");
-    client.funcs.log(`Unchanged roles : ${rolesUnchanged}`, "debug");
-    client.funcs.log("START OF USER RESPONSE", "debug");
     msg.channel.stopTyping(true);
-    if (isit("non-empty array", rolesChanged)) { // roles.some((element) => { return operation === "add" ? !element.hasAlready : element.hasAlready; })
+    if (isit("non-empty array", rolesChanged)) {
       const rlsChngd = rolesChanged.join(", ");
       const rlChngdmore1 = rolesChanged.length > 1;
       const rlsUnchngd = rolesUnchanged.join(", ");
@@ -157,10 +122,8 @@ module.exports = (client, msg, operation, role, roles, possibleRoles) => {
 
   // add roles
   if (isit("non-empty array", rolesToAdd)) {
-    client.funcs.log("Adding roles", "debug");
-    msg.member.addRoles(rolesToAdd).then((member) => { // eslint-disable-line no-unused-vars
-      /* client.funcs.log(`member.roles.size = ${member.roles.size}`, "debug");
-      if (!rolesToAdd.every((roleToAdd) => { return member.roles.some((r) => { return roleToAdd.id === r.id; }); })) {
+    msg.member.addRoles(rolesToAdd).then(() => {
+      /* if (!rolesToAdd.every((roleToAdd) => { return member.roles.some((r) => { return roleToAdd.id === r.id; }); })) {
         msg.channel.stopTyping(true);
         throw new Error("Failed to add one or more roles.");
       } else {*/
@@ -171,9 +134,8 @@ module.exports = (client, msg, operation, role, roles, possibleRoles) => {
       msg.channel.stopTyping(true);
       throw e;
     });
-  } else if (isit("non-empty array", rolesToRemove)) { // make remove like add
-    client.funcs.log("Adding roles", "debug");
-    msg.member.removeRoles(rolesToRemove).then((member) => { // eslint-disable-line no-unused-vars
+  } else if (isit("non-empty array", rolesToRemove)) {
+    msg.member.removeRoles(rolesToRemove).then(() => { // eslint-disable-line no-unused-vars
       /* if (!rolesToRemove.every((roleToRemove) => { return member.roles.some((r) => { return roleToRemove.id === r.id; }); })) {
         msg.channel.stopTyping(true);
         throw new Error("Failed to add one or more roles.");
@@ -190,6 +152,4 @@ module.exports = (client, msg, operation, role, roles, possibleRoles) => {
   }
 
   msg.channel.stopTyping(true);
-  client.funcs.log("END OF COMMAND", "debug");
-  client.funcs.log("¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦", "debug");
 };
