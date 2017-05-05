61c574410e3f8e1679ff5e092f465668c8331516
Damian Blume <damian.blume@gmail.com>
2017-05-05 23:39:12 +0200

    Changed announce commands to accept text or a link

--- commands/moderation/announce.js
+++ commands/moderation/announce.js
@@ -1,15 +1,19 @@
 const request = require("request-promise-native");
 
-exports.run = (client, msg, [channel, url]) => {
-  client.funcs.log(url, "debug");
-  request(url)
+exports.run = (client, msg, [channel, input]) => {
+  if (input.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g)) {
+    const url = input;
+    request(url)
     .then((resp) => {
-      client.funcs.log(resp);
       if (typeof resp !== "string" || resp.length < 1 || resp.length > 2000) msg.reply("The content of the page you linked is not valid");
       else channel.send(resp).catch(() => { msg.reply("Error : can't send message"); });
     }).catch((e) => {
       throw new Error(`Problem with link. Error : ${e.name} ${e.statusCode}`);
     });
+  } else {
+    const messageToSend = msg.content.slice(msg.content.indexOf(channel.toString()) + channel.toString().length);
+    channel.send(messageToSend).catch(() => { throw new Error("Error: can't send message"); });
+  }
 };
 
 exports.conf = {
@@ -24,7 +28,7 @@ exports.conf = {
 exports.help = {
   name: "announce",
   description: "Sends a message in a specific channel",
-  usage: "<channel:channel> <content:url>",
+  usage: "<channel:channel> <content:string>",
   usageDelim: " ",
-  extendedHelp: "Usage : Mention a channel and then put a raw text link",
+  extendedHelp: "Usage: Mention a channel and then put a raw text link or just your message",
 };
--- commands/moderation/editAnnouncement.js
+++ commands/moderation/editAnnouncement.js
@@ -1,15 +1,21 @@
 const request = require("request-promise-native");
 
-exports.run = (client, msg, [targetMsg, url]) => {
-  if (targetMsg.author.id !== client.user.id) throw new Error("Can't edit message");
+exports.run = (client, msg, [targetMsg, input]) => {
+  if (targetMsg.author.id !== client.user.id) throw new Error("I can't edit this message");
 
-  request(url)
+  if (input.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g)) {
+    const url = input;
+    request(url)
     .then((resp) => {
       if (typeof resp !== "string" || resp.length < 1 || resp.length > 2000) msg.reply("The content of the page you linked is not valid");
       else targetMsg.edit(resp).catch(() => { msg.reply("Error : can't edit message"); });
     }).catch((e) => {
-      throw new Error(e);
+      throw new Error(`Problem with link. Error : ${e.name} ${e.statusCode}`);
     });
+  } else {
+    const messageToSend = msg.content.slice(msg.content.indexOf(targetMsg.toString()) + targetMsg.toString().length);
+    targetMsg.edit(messageToSend).catch(() => { throw new Error("Error: can't edit message"); });
+  }
 };
 
 exports.conf = {
@@ -24,7 +30,7 @@ exports.conf = {
 exports.help = {
   name: "edit",
   description: "Edits one of my mesages",
-  usage: "<message:message> <content:url>",
+  usage: "<message:message> <content:string>",
   usageDelim: " ",
   extendedHelp: "Usage : put a message ID (only my messages tho) and then put a raw text link",
 };
