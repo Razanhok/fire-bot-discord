f5fc210e52d84c205c03f1e5b39b3ca130d70d0f
Damian Blume <damian.blume@gmail.com>
2017-05-08 22:44:25 +0200

    Message now links to commands channel

--- inhibitors/channelLock.js
+++ inhibitors/channelLock.js
@@ -6,7 +6,7 @@ exports.conf = {
 exports.run = (client, msg) => {
   if (msg.channel.type === "text") {
     if (msg.guild.id === "293161643674828800" && msg.channel.id !== "293163428061642764") {
-      return "Commands can only be used in the commands channel";
+      return "Commands can only be used in the <#311241522852921354> channel";
     }
   }
   return false;
